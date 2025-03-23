be89bdce4212df5512b9052255394ff785927f0c
Giovanni Mazzone <giovannimariamazzone@gmail.com>
2025-03-23 22:48:46 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import fetch from "node-fetch";
+import { getFinalDownloadUrl } from "./mirror-dl";
+const app = express();
+
+app.get("/download", async (req, res) => {
+  const { ver } = req.query;
+
+  let finalDlUrl = "";
+  switch (ver) {
+    case "25.3.0":
+      finalDlUrl =
+        "https://www.apkmirror.com/apk/grindr-llc/grindr-gay-chat-meet-date/grindr-gay-dating-chat-25-3-0-release/grindr-gay-dating-chat-25-3-0-android-apk-download/";
+  }
+
+  if (finalDlUrl === "") {
+    res.status(404).send("Not found");
+    return;
+  }
+
+  const fetchResponse = await fetch(await getFinalDownloadUrl(finalDlUrl));
+  if (fetchResponse.body === null) {
+    res.status(404).send("Not found");
+    return;
+  }
+
+  res.status(200);
+  res.header("Content-Type", "application/vnd.android.package-archive");
+  res.header("Content-Disposition", "attachment; filename=grindr.apk");
+
+  fetchResponse.body.pipe(res);
+});
+
+app.listen(3000, () => {
+  console.log(`App listening on port 3000`);
+});
---
+++ mirror-dl.ts
@@ -0,0 +1,38 @@
+import { load } from "cheerio";
+
+export function getFinalDownloadUrl(downloadPageUrl: string) {
+  return fetch(downloadPageUrl)
+    .then((res) => res.text())
+    .then(extractRedirectDownloadUrl)
+    .then(withBaseUrl)
+    .then((url) => fetch(url))
+    .then((res) => res.text())
+    .then(extractFinalDownloadUrl)
+    .then(withBaseUrl);
+}
+
+export function extractRedirectDownloadUrl(downloadPageHtml: string) {
+  const $ = load(downloadPageHtml);
+  const downloadUrl = $(`a.downloadButton`).attr("href");
+  if (!downloadUrl) {
+    throw new Error("Could not find redirect download url");
+  }
+  return downloadUrl;
+}
+
+export function extractFinalDownloadUrl(downloadPageHtml: string) {
+  const $ = load(downloadPageHtml);
+  const downloadUrl = $(`.card-with-tabs a[href]`).attr("href");
+  if (!downloadUrl) {
+    throw new Error("Could not find final download url");
+  }
+  return downloadUrl;
+}
+
+function withBaseUrl(endpoint: string) {
+  if (endpoint.startsWith("/")) {
+    return `https://www.apkmirror.com${endpoint}`;
+  }
+
+  return `https://www.apkmirror.com/${endpoint}`;
+}
